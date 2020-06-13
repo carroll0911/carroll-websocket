96b0ba17204d886d8598516fa978bae9860373c7
hongbo.he <hongbo.he@timanetworks.com>
2020-06-13 09:00:04 +0800

    init

--- src/main/java/com/carroll/websocket/WebSocketConfig.java
+++ src/main/java/com/carroll/websocket/WebSocketConfig.java
@@ -17,7 +17,7 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/endpointWisely").withSockJS();
+        registry.addEndpoint("/endpointWisely").setAllowedOrigins("*").withSockJS();
     }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
